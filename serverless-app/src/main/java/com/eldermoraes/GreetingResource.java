7aa14fed00c8cc9029aab958470729caadb57c28
Elder Moraes <elder@eldermoraes.com>
2024-10-04 15:45:45 -0300

    Na prática

--- serverless-app/src/main/java/com/eldermoraes/GreetingResource.java
+++ serverless-app/src/main/java/com/eldermoraes/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
 
     private static int count = 0;
 
-    @ConfigProperty(name = "msg", defaultValue = "Aula, ")
+    @ConfigProperty(name = "msg", defaultValue = "Aula , ")
     private String msg;
 
     @GET
